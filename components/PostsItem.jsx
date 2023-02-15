c2fa46378d4c16835298e9308992f4329cefb1d6
Muhammad Zunnoorain Rafi <108727664+MuhammadZunnoorainRafi@users.noreply.github.com>
2023-02-15 15:28:16 +0500

    doneee

--- components/PostsItem.jsx
+++ components/PostsItem.jsx
@@ -2,14 +2,14 @@ import Image from 'next/image';
 import Link from 'next/link';
 import Category from './Category';
 
-const PostsItem = ({ post,compact }) => {
+const PostsItem = ({ post, compact }) => {
   const { title, date, excerpt, category, cover_image, author, author_image } =
     post.frontMatter;
   return (
     <div>
       <div className="p-5 border border-slate-300 h-full flex flex-col justify-between  rounded-lg shadow-lg ">
         <div>
-       {!compact &&  <div className=" rounded-lg overflow-hidden">
+          <div className=" rounded-lg overflow-hidden">
             <Image
               src={cover_image}
               height={420}
@@ -18,7 +18,7 @@ const PostsItem = ({ post,compact }) => {
               priority
               alt="error"
             />
-          </div> }
+          </div>
           <div className="flex items-center justify-between py-2">
             <p className="text-slate-600 text-sm ">{date}</p>
             <Category>{category}</Category>
@@ -31,22 +31,24 @@ const PostsItem = ({ post,compact }) => {
           </Link>
           <p className="text-slate-600 pt-1 pb-4">{excerpt}</p>
         </div>
-      {!compact &&  <div className="flex items-center justify-between mt-6">
-          <Link
-            href={`/blogs/${post.slug}`}
-            className=" text-blue-500 hover:text-blue-800  "
-          >
-            Read More
-          </Link>
-          <picture className="flex items-center justify-center space-x-2">
-            <img
-              src={author_image}
-              alt="error"
-              className=" h-7 w-7 object-cover rounded-full"
-            />
-            <p className="font-semibold text-sm">{author}</p>
-          </picture>
-        </div> }
+        {!compact && (
+          <div className="flex items-center justify-between mt-6">
+            <Link
+              href={`/blogs/${post.slug}`}
+              className=" text-blue-500 hover:text-blue-800  "
+            >
+              Read More
+            </Link>
+            <picture className="flex items-center justify-center space-x-2">
+              <img
+                src={author_image}
+                alt="error"
+                className=" h-7 w-7 object-cover rounded-full"
+              />
+              <p className="font-semibold text-sm">{author}</p>
+            </picture>
+          </div>
+        )}
       </div>
     </div>
   );
